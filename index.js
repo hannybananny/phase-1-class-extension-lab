bfe5a016446339047f907329c0367ae731987795
Hannah <hannahmgrout@gmail.com>
2022-05-19 20:09:21 -0700

    commit tree 88f8c96df6e052c5d4f95e32aaf12c7129baef91

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Your code here
+class Polygon {
+    constructor(arrOfInt){
+        this.arrOfInt = arrOfInt
+    }
+        get countSides(){
+           return this.arrOfInt.length 
+       } 
+       get perimeter(){
+            return this.arrOfInt.reduce((accumulator, curr) => accumulator + curr)
+       }
+    }
+
+class Triangle extends Polygon {
+    get isValid(){
+        let [a, b, c] = this.arrOfInt
+
+        if((a+b)>c && (a+c)>b && (b+c)>a){
+            return true
+        }
+        else{
+            return false
+        }
+      }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        let [a, b, c ,d] = this.arrOfInt
+    
+        if(a === b &&  b === c && c === d){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+
+    get area(){
+        let [a, b] = this.arrOfInt
+        return a * b
+    }
+}
